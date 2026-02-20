bdbc0c7ada16193f61051ae398f8dc948c508a21
Fitz-Codes <michaelfitz24@icloud.com>
2026-02-19 19:40:00 -0500

    Added Assets and JS needed for first song

---
+++ music-player.js
@@ -0,0 +1,17 @@
+const songs = [
+
+    {
+        image: "./Malcolm Todd Cover.jpg",
+        name: "Out of Bounds",
+        artist: "Malcolm Todd",
+        audio: "./Malcolm Todd - Out of Bounds (SPOTISAVER).mp3",
+    },
+
+    {
+
+    },
+
+    {
+
+    }
+];
